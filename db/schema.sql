8647bbe978529170e3b0ec38f4bbf7bdffce7808
annammattis <54290779+annammattis@users.noreply.github.com>
2020-03-12 14:52:47 -0400

    first draft

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE burgers_db;
+
+-- USE burgers_db;
+
+-- CREATE TABLE burgers (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     burger_name VARCHAR(100),
+--     devoured BOOLEAN,
+--     PRIMARY KEY (id)
+-- );
+
+-- INSERT INTO burgers (burger_name, devoured)
+-- VALUES ("Avocali Burger", TRUE);
+
+-- INSERT INTO burgers (burger_name, devoured)
+-- VALUES ("Texmex Burger", FALSE);
+
+-- INSERT INTO burgers (burger_name, devoured)
+-- VALUES ("Bacon and Bleu Burger", TRUE);
+
+-- INSERT INTO burgers (burger_name, devoured)
+-- VALUES ("Teryaki Chili Burger", TRUE);
+
+-- use burgers;
