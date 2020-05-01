f45758560509d5039814467c58ae97b1969e5aae
annammattis <54290779+annammattis@users.noreply.github.com>
2020-04-30 21:47:46 -0400

    restructured models

--- models/burger.js
+++ models/burger.js
@@ -1,38 +1,15 @@
-// Inside burger.js, import orm.js into burger.js
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
-// Export at the end of the burger.js file.
+// Burger models
 
+// The burger has a burger_name attribute of type DataTypes.String
+// and a devoured attribute that is false by default
 
 module.exports = function(sequelize, DataTypes) {
-  var Burgers = sequelize.define("Burgers", {
-    text: DataTypes.STRING,
-    complete: DataTypes.BOOLEAN
+  var Burger = sequelize.define("Burger", {
+    burger_name: DataTypes.STRING,
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
   });
-  return Burgers;
+  return Burger;
 };
-
-
-// // Import the ORM to create functions that will interact with the database.
-// var orm = require("../config/orm.js");
-
-// var burger = {
-//   selectAll: function(cb) {
-//     orm.selectAll("burgers", function(res) {
-//       cb(res);
-//     });
-//   },
-//   // The variables cols and vals are arrays.
-//   create: function(cols, vals, cb) {
-//     orm.create("burgers", cols, vals, function(res) {
-//       cb(res);
-//     });
-//   },
-//   update: function(objColVals, condition, cb) {
-//     orm.update("burgers", objColVals, condition, function(res) {
-//       cb(res);
-//     });
-//   }
-// };
-
-// // Export the database functions for the controller (catsController.js).
-// module.exports = burger;
